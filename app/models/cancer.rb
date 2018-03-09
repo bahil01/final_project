be3d4242ab6bf7d23ccfa106b3848a47bb33585d
Raghu Betina <raghu@firstdraft.com>
2018-03-09 23:20:04 +0000

    Direct association between cancers and treatments

--- app/models/cancer.rb
+++ app/models/cancer.rb
@@ -1,6 +1,9 @@
 class Cancer < ApplicationRecord
   # Direct associations
 
+  has_many   :treatments,
+             :dependent => :destroy
+
   belongs_to :user
 
   # Indirect associations
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,6 +1,8 @@
 class Treatment < ApplicationRecord
   # Direct associations
 
+  belongs_to :cancer
+
   # Indirect associations
 
   # Validations
