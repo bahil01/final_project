3672f13c52fc1bb13d6bff8a5aeb823190532370
Raghu Betina <raghu@firstdraft.com>
2018-03-11 05:18:43 +0000

    2nd draft

--- app/admin/treatments.rb
+++ app/admin/treatments.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Treatment do
 
- permit_params :cancer_id, :treatment_name
+ permit_params :user_id, :treatment_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
--- db/migrate/20180311051706_create_treatments.rb
+++ db/migrate/20180311051706_create_treatments.rb
@@ -1,7 +1,7 @@
 class CreateTreatments < ActiveRecord::Migration
   def change
     create_table :treatments do |t|
-      t.integer :cancer_id
+      t.integer :user_id
       t.string :treatment_name
 
       t.timestamps
